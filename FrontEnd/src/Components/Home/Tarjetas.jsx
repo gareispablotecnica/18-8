32cf42b43a8e3f0cd07e84867b346afaa5070dea
Gareis Pablo <gareispablotecnica1@gmail.com>
2026-09-07 00:56:01 -0300

    CSS

--- FrontEnd/src/Components/Home/Tarjetas.jsx
+++ FrontEnd/src/Components/Home/Tarjetas.jsx
@@ -1,45 +1,68 @@
-// rfec + Enter
-import React from 'react'
-// --> Dependencias
-import { useState, useEffect } from 'react'
-// ---> services/api.js --> metodo
-import { api } from '../../Services/api'
-
+import React, { useState, useEffect } from 'react';
+import { api } from '../../Services/api';
 
 function Tarjetas() {
-    // --> BD:V , Capturamos
+
     const [BaseDatos, setBaseDatos] = useState([]);
+
     useEffect(() => {
-        // --> Metodo para ver tablas
+
         const VerTablas = async () => {
-            // si
+
             try {
-                // capturamos la url
+
                 const buscar = await api.get('/verTabla');
-                // ---> data: info
+
                 setBaseDatos(buscar.data);
-            }
-            catch (error) {
+
+            } catch (error) {
+
                 console.error('Error fetching data:', error);
+
             }
+
         };
+
         VerTablas();
+
     }, []);
 
-
     return (
-        <>
-            <div className='Planetes'>
-                {BaseDatos.map((i)=>(
-                    <div key={i.ID_Planeta} className='Tarjeta'>
-                        <h1 className='Nombre'>{i.Nombre}</h1>
-                        <h2 className='Descripcion'>{i.Descripcion}</h2>
-                        <h3 className='Dis'>{i.Dis}</h3>
+        <div className="Planetes">
+
+            {BaseDatos.map((i, index) => (
+
+                <article
+                    key={i.ID_Planeta}
+                    className="Tarjeta"
+                >
+
+                    <span className="NumeroPlaneta">
+                        0{index + 1}
+                    </span>
+
+                    <div>
+
+                        <h2 className="Nombre">
+                            {i.Nombre}
+                        </h2>
+
+                        <p className="Descripcion">
+                            {i.Descripcion}
+                        </p>
+
+                        <p className="Dis">
+                            {i.Dis}
+                        </p>
+
                     </div>
-                ))}
-            </div>
-        </>
-    )
+
+                </article>
+
+            ))}
+
+        </div>
+    );
 }
 
-export default Tarjetas
+export default Tarjetas;
